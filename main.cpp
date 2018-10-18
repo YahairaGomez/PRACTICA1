b54f1eedc72b9f2a81da2d97b2cdc3474c21cfb8
YahairaGomez <42581200+YahairaGomez@users.noreply.github.com>
2018-10-18 09:47:48 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,90 +1,49 @@
 #include <iostream>
-#include <cmath>
 #include <stdlib.h>
+#include <math.h>
+
 
 using namespace std;
-class Punto
-{
-    public:
-       int x,y ;
-
+class Punto{
+public:
+    int x,y;
 };
 
-void imprimir(Punto arr[], int tam) {
-for (int i=0; i< tam; i++){
-    cout<<"("<<arr[i].x<<"," <<arr[i].y<<")"<<" ";
-}
-}
-bool creciente(int c1, int c2){
-    return c1>  c2;
-}
-
-bool decreciente(int c1, int c2){
-    return c1<  c2;
-}
-
-void ordenar(Punto arr[], int tam, bool(*criterio)(int, int)){
-for (int i=0; i<tam; i++){
-            for(int j=0; j<tam; j++){
-                if ((*criterio)(arr[j].x, arr[i].x)){
-                    Punto aux=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=aux;
-                }
-            }
-
-
-    }
-}
-
-////// Ordenar nombres
-class Alumno
-{
-    public:
-       char* name;
-       int ID;
-
+class Vector{
+public:
+    Punto inicio, fin;
 };
+void imprimir(const Vector *v){
+    cout<<"("<< (*v).inicio.x<<"," << (*v).inicio.y << ") -> (" <<(*v).fin.x<<","<<(*v).fin.y<<")"<<endl;//podemos poner v ->
+}
+float modulo(Vector vec){
+    return sqrt(pow((vec.fin.x)-(vec.inicio.x),2 )+ pow((vec.fin.x)-(vec.inicio.x),2));
+}
 
 
 int main()
-{   /*Punto p;
-    p.x=5;
-    p.y=4;
-    cout<<"("<<p.x<<"," <<p.y<<")"<<endl;*/
+{
+    Vector vec; //vector de nombre vec
+    vec.inicio.x= 4;
+    vec.inicio.y= 5;
+    vec.fin.x= 2;
+    vec.fin.y= 6;
+    //imprimir(vec);
+    //cout<<modulo(vec);
 
-    int n=10;
-    Punto arr[n];
+    int n=4;
+    Vector arr[n];
     for (int i=0;i<n; i++){
-        arr[i].x = rand() %100;
-        arr[i].y=  rand() %100 ;;
-       // cout<<"("<<arr[i].x<<"," <<arr[i].y<<")"<<" ";
+        arr[i].inicio.x = rand() %10;
+        arr[i].inicio.y=  rand() %10 ;
+}
 
+    for(int i=0; i<n; i++){
+        //cout<<modulo(arr[i])<<" ";
 
     }
-    cout<<endl;
-    //ordenar
-    ordenar(arr,n,decreciente);
-    imprimir(arr,n);
 
-    Alumno student1;
-    student1. name= "Valeria";
-    student1. ID=12131;
-
-    Alumno student12;
-    student2. name= "Yahaira";
-    student2. ID=121365;
-
-    Alumno student1;
-    student3. name= "Sebas";
-    student3. ID=121315987;
-
-    int n=convertir(student1.name);
-    int m=convertir(student2.name);
-
-
-
-
-
+    Vector *ptr=&vec;
+    imprimir(ptr);
     return 0;
 }
